e22ca44d3541fb193fb59657fe3b0295fa516014
Diogo2584 <penafieldp8@gmail.com>
2026-01-16 09:07:23 -0500

    updated changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -29,7 +29,7 @@ function About(){
             <p className="caption1"> OUR STORY</p>
         </div>
 
-        <div className={styles.rightColumnContainer}>
+        
             <div className={styles.AuthorInformation}>
                 <picture>
                     <source media = "(min-width: 1200px)" srcSet= {lady} />
@@ -41,29 +41,24 @@ function About(){
                     <p className="heading4"> Owner</p>
                 </div>
             </div>
+            <div className={styles.rightColumnContainer}>
             <p className="heading2"> Our Blooms was founded in honor of Lily Smith’s loving aunts, Teresa and Beth. </p>
             <div className={styles.imgContainer}>
-                <div className={styles.img1}>
                 <picture>
                     <source media= "(min-width: 1200px)" srcSet={pic1a}/>
                     <source media= "(min-width: 800px)" srcSet={pic1b}/>
                     <img src={pic1c} alt="Our Blooms Image 1"/>
                 </picture>
-                </div>
-                <div className={styles.img2}>
                 <picture>
                     <source media= "(min-width: 1200px)" srcSet={pic2a}/>
                     <source media= "(min-width: 800px)" srcSet={pic2b}/>
                     <img src={pic2c} alt="Our Blooms Image 2"/>
                 </picture>
-                </div>
-                <div className={styles.img3}>
                 <picture>
                     <source media= "(min-width: 1200px)" srcSet={pic3a}/>
                     <source media= "(min-width: 800px)" srcSet={pic3b}/>
                     <img src={pic3c} alt="Our Blooms Image 3"/>
                 </picture>
-                </div>
             </div>
             <div className={styles.Journey}>
             <p className="paragraph2">  Lily’s journey with flowers began in the heart of Oregon, amidst the flourishing fields of her aunts' flower farm. It was there, surrounded by the abundance of nature, that she discovered her passion for floral design. From learning the names of each bloom to understanding the delicate balance of a bouquet, she absorbed the artistry of flowers like the rich Oregon soil. </p>
@@ -76,14 +71,14 @@ function About(){
                 <img src={pic4c} alt="Our Blooms Image Breaker"/>
             </picture>
             </div>
-            <div className={styles.begin}>
+            <div className={styles.Journey}>
             <p className="paragraph2"> From humble beginnings, Bloom&Co has grown into a beloved local destination, known for its artistic arrangements, personal service, and commitment to quality. </p>
             <p className="paragraph2"> Discover how we can add a touch of natural beauty to your next event. </p>
             </div>
             <button className="button_danger">.BOOK A CONSULTATION</button> 
-            <Footer/>
+            
         </div>
-       
+        <Footer/>
         
         
         
